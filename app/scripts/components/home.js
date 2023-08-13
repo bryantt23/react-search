b750cf8c9f55cbf8176331c4c7d8db4780a5500d
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-08-12 23:16:30 -0700

    use express, show data

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -2,21 +2,83 @@
  * This file will hold the Main content that lives in the main body of the site
  *
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+function getFacetsArray(checkedItems) {
+  return Object.entries(checkedItems)
+    .filter(([key, value]) => value) // only include items that are checked
+    .map(([key, value]) => `${encodeURIComponent(key)}`);
+}
 
 function Home() {
-  /**
-   * Renders the default app in the window, we have assigned this to an element called root.
-   *
-   * @returns JSX
-   * @memberof Home
-   */
+  const [data, setData] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [checkedItems, setCheckedItems] = useState({
+    Samsung: false,
+    Apple: false,
+    Motorola: false,
+    Watch: false,
+    Tablet: false,
+    Phone: false
+  });
+  useEffect(() => {
+    console.log(checkedItems);
+    async function fetchData() {
+      // Convert checkedItems object to query string
+      let facetsArray = getFacetsArray(checkedItems),
+        facetsUrl = '';
+
+      if (facetsArray.length > 0) {
+        facetsUrl = `facets=${facetsArray.join(',')}`;
+      }
+
+      try {
+        const res = await fetch(
+          `http://localhost:3035/store?searchText=${searchText}&${facetsUrl}`
+        );
+        const json = await res.json();
+        console.log('🚀 ~ file: home.js:57 ~ fetchData ~ json:', json);
+        setData(json);
+      } catch (error) {}
+    }
+
+    fetchData();
+  }, [checkedItems, searchText]);
+
+  const handleChange = event => {
+    setCheckedItems({
+      ...checkedItems,
+      [event.target.name]: event.target.checked
+    });
+  };
+
   return (
-    <section id='home'>
-      <div className='content'>
-        <p>ELC Coding Test...</p>
+    <div style={{ padding: 10 }}>
+      <p>About {data.length} results</p>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          gap: '10px'
+        }}
+      >
+        {data.map(item => (
+          <div
+            key={item.id}
+            style={{
+              border: '1px solid black',
+              width: 200,
+              height: 200
+            }}
+          >
+            <p>
+              {item.brand} {item.model}
+            </p>
+            <img style={{ maxHeight: 100 }} src={item.picture} />
+          </div>
+        ))}
       </div>
-    </section>
+    </div>
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 /**
  * The Server Can be configured and created here...
- * 
+ *
  * You can find the JSON Data file here in the Data module. Feel free to impliment a framework if needed.
  */
 
@@ -19,26 +19,60 @@
     ]
 }
 */
-const data      = require('./data');
-const http      = require('http');
-const hostname  = 'localhost';
-const port      = 3035;
+const data = require('./data');
+const http = require('http');
+const hostname = 'localhost';
+const port = 3035;
+const express = require('express');
+const cors = require('cors');
 
-/** 
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+/**
  * Start the Node Server Here...
- * 
- * The http.createServer() method creates a new server that listens at the specified port.  
- * The requestListener function (function (req, res)) is executed each time the server gets a request. 
+ *
+ * The http.createServer() method creates a new server that listens at the specified port.
+ * The requestListener function (function (req, res)) is executed each time the server gets a request.
  * The Request object 'req' represents the request to the server.
  * The ServerResponse object 'res' represents the writable stream back to the client.
  */
-http.createServer(function (req, res) {
-    // .. Here you can create your data response in a JSON format
-    
-    
-    res.write("Response goes in here..."); // Write out the default response
-    res.end(); //end the response
-}).listen( port );
+app.get('/store', (req, res) => {
+  res.json(data);
+  return data;
+  const facets = req.query.facets;
+  const searchText = req.query.searchText.toLowerCase();
 
+  const resultsFromSearchText = data.filter(
+    item => item.model.toLowerCase().indexOf(searchText) > -1
+  );
+
+  if (!facets) {
+    const dataWithPosition = resultsFromSearchText.map((item, index) => ({
+      ...item,
+      position: index
+    }));
+    res.json(dataWithPosition);
+    return;
+  }
+
+  const filteredResultsWithPosition = resultsFromSearchText
+    .filter(
+      item => facets.indexOf(item.brand) > -1 || facets.indexOf(item.type) > -1
+    )
+    .map((item, index) => ({
+      ...item,
+      position: index
+    }));
+
+  res.json(filteredResultsWithPosition);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 console.log(`[Server running on ${hostname}:${port}]`);
