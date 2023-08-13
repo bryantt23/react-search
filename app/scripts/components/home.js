a28b2dad6c361e0cb7631add9c3a6ceb6409b847
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-08-12 22:50:15 -0700

    convert to functional component

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -1,29 +1,23 @@
 /**
  * This file will hold the Main content that lives in the main body of the site
- * 
+ *
  */
 import React from 'react';
 
-
-class Home extends React.Component {
-
-    /**
-     * Renders the default app in the window, we have assigned this to an element called root.
-     * 
-     * @returns JSX
-     * @memberof Home
-    */
-    render() {
-        return (
-            <section id="home">
-                <div className="content">
-                    <p>ELC Coding Test...</p>
-                </div>
-            </section>
-        );
-    }
-
-
+function Home() {
+  /**
+   * Renders the default app in the window, we have assigned this to an element called root.
+   *
+   * @returns JSX
+   * @memberof Home
+   */
+  return (
+    <section id='home'>
+      <div className='content'>
+        <p>ELC Coding Test...</p>
+      </div>
+    </section>
+  );
 }
 
 // Export out the React Component
