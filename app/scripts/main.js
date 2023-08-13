740185f523a04ce45a7893b88ac5f0d03562b695
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-08-12 23:17:53 -0700

    to functional

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,21 +21,19 @@ import Home from './components/home';
 /**
  * We can start our initial App here in the main.js file
  */
-class App extends React.Component {
+function App() {
   /**
    * Renders the default app in the window, we have assigned this to an element called root.
    *
    * @returns JSX
    * @memberof App
    */
-  render() {
-    return (
-      <div className='App'>
-        <Menu />
-        <Home />
-      </div>
-    );
-  }
+  return (
+    <div className='App'>
+      <Menu />
+      <Home />
+    </div>
+  );
 }
 
 // Render this out
